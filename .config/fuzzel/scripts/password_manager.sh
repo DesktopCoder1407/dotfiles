6a175489cd5f7bed3f2b027521c9cfa6d3ec6a6b
DesktopCoder1407 <>
2026-01-17 15:30:30 -0500

    feat: implement fuzzel

---
+++ .config/fuzzel/scripts/password_manager.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+item=$(rbw list | fuzzel -d --placeholder "ALT+0 to Sync | ALT+1 for Username")
+exit_code=$?
+
+# ALT+0 for Sync
+if [ "$exit_code" -eq 19 ]; then
+	rbw sync && ~/.config/fuzzel/scripts/password_manager.sh
+# Close if no item selected
+elif [ -z "$item" ]; then
+	exit
+# ALT+1 for Username
+elif [ "$exit_code" -eq 10 ]; then
+	rbw search "$item" --fields user | wl-copy
+else
+	pass=$(rbw get "$item")
+	echo $pass | wl-copy
+	cliphist delete-query $pass
+fi
--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Required Packages:
 `vim`
 `hyprland`
+`fuzzel`
 `cliphist`
 `xdg-desktop-portal-wlr`
 `xdg-desktop-portal-hyprland`
 
 # Optional Packages:
-`iwd` - Wireless Support
 `pipewire-pulse` - Audio Support - Pulse
 `pipewire-jack` - Audio Support - Jack
 `pipewire-alsa` - Audio Support - Alsa
+`iwd` - Wireless Support
+`rbw` - Bitwarden CLI
 
 # Boot Packages:
 `base`
