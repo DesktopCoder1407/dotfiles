5341bfac4b66dc66239eaee604368e162f145698
DesktopCoder1407 <>
2026-01-19 00:23:54 -0500

    feat: rewrite clipboard script and add clearing

--- .config/fuzzel/scripts/password_manager.sh
+++ .config/fuzzel/scripts/password_manager.sh
@@ -3,16 +3,16 @@
 item=$(rbw list | fuzzel -d --placeholder "ALT+0 to Sync | ALT+1 for Username")
 exit_code=$?
 
+# Do nothing if no selection
+[ -z "$item" ] && exit
+
 # ALT+0 for Sync
 if [ "$exit_code" -eq 19 ]; then
 	rbw sync && ~/.config/fuzzel/scripts/password_manager.sh
-# Close if no item selected
-elif [ -z "$item" ]; then
-	exit
 # ALT+1 for Username
 elif [ "$exit_code" -eq 10 ]; then
 	rbw search "$item" --fields user | wl-copy
-else
+else  # Copy Pasword
 	pass=$(rbw get "$item")
 	echo $pass | wl-copy
 	cliphist delete-query $pass
